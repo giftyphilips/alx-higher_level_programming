593e5c20a485964f8f651593a16647d2dda6ba27
Gifty <giftyikechukwu@gmail.com>
2022-10-12 04:07:52 +0100

    Add files via upload

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-square.py
+"""defines a class Square"""
+
+
+class Square:
+    """ class for Square"""
+    def __init__(self, size):
+        """ Initialize class """
+        self.__size = size
